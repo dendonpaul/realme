234319dd2c6538f9d670d59eed17c53e23890507
dendonpaul <dendonpaul@gmail.com>
2023-01-21 21:59:49 +0530

    added option to run specific scripts based on the pages

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 /** Circle effect on homepage with button click**/
-let circle = document.getElementById("circle");
+const url = window.location.href;
+const lastSegment = url.split("/").pop();
 
-let btnUp = document.getElementById("btnUp");
-let btnDown = document.getElementById("btnDown");
+if (lastSegment === "index.html") {
+  let circle = document.getElementById("circle");
 
-let rotateValue = circle.style.transform;
-console.log(rotateValue);
-let rotateSum;
+  let btnUp = document.getElementById("btnUp");
+  let btnDown = document.getElementById("btnDown");
 
-function upClick() {
-  rotateSum = rotateValue + "rotate(-90deg)";
-  circle.style.transform = rotateSum;
-  rotateValue = rotateSum;
-}
+  let rotateValue = circle.style.transform;
+  console.log(rotateValue);
+  let rotateSum;
 
-function downClick() {
-  rotateSum = rotateValue + "rotate(90deg)";
-  circle.style.transform = rotateSum;
-  rotateValue = rotateSum;
-}
+  function upClick() {
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+  }
 
-/*Contact Form*/
-/*Form Validation*/
+  function downClick() {
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+  }
+} else if (lastSegment === "contact.html") {
+  /*Contact Form*/
+  /*Form Validation*/
 
-//Validate Name Field
-function validate() {
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("InputEmail");
-  const subjectInput = document.getElementById("subject");
-  const messageInput = document.getElementById("message");
-  //check if empty
-  if (
-    nameInput.value == "" ||
-    emailInput.value == "" ||
-    subjectInput.value == "" ||
-    messageInput.value == ""
-  ) {
-    alert("Field cannot be empty");
-    //check if name field has more that 6 chars
-  } else if (nameInput.value.length < 6) {
-    alert("Need 6 or more chars.");
-    //check if name field has 20 or less chars
-  } else if (nameInput.value.length > 20) {
-    alert("Only 20 Chars allowed.");
-    //success alert if validation is successful
-  } else {
-    alert("Form Validated Successfully.");
+  //Validate Name Field
+  function validate() {
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("InputEmail");
+    const subjectInput = document.getElementById("subject");
+    const messageInput = document.getElementById("message");
+    //check if empty
+    if (
+      nameInput.value == "" ||
+      emailInput.value == "" ||
+      subjectInput.value == "" ||
+      messageInput.value == ""
+    ) {
+      alert("Field cannot be empty");
+      //check if name field has more that 6 chars
+    } else if (nameInput.value.length < 6) {
+      alert("Need 6 or more chars.");
+      //check if name field has 20 or less chars
+    } else if (nameInput.value.length > 20) {
+      alert("Only 20 Chars allowed.");
+      //success alert if validation is successful
+    } else {
+      alert("Form Validated Successfully.");
+    }
   }
 }
